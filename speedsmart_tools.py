fa24a3bf893e5a9c58133b817224b5807741e7b7
Legendusmaximus <tapsandsinkstheo@gmail.com>
2022-08-03 12:17:12 +0100

    just found out itteration is not by row, trying again

--- speedsmart_tools.py
+++ speedsmart_tools.py
@@ -1,9 +1,20 @@
 import csv
 
-def restore_full_length(tablename):
-    filename = tablename
-    with open(filename, "r") as csvfile:
-        datareader = csv. reader(csvfile)
-        for row in datareader:
-            print(row)
-            input()
+def restore_full_length(originaltable, latesttable):
+    # This function will give you a full-length SpeedSmart table
+    with open(originaltable, "r") as ss_original_file:
+        with open (latesttable, "r") as ss_latest_file:
+            # Remove the headers
+            ss_latest_file.readline()
+            # Get the first speedtest present in the file
+            firsttest = ss_latest_file.readline()
+            firsttest = firsttest.split(",").pop(0)
+            count = 1
+            for row in ss_original_file:
+                row = row.split(",").pop(0)
+                print(row)
+                print(firsttest)
+                input()
+                if row == firsttest:
+                    print("Found first test in original file, row "+count)
+                    count += 1
