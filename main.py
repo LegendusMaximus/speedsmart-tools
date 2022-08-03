27b1a928b76bf168952468412004a3e0b9df4a3c
Legendusmaximus <tapsandsinkstheo@gmail.com>
2022-08-03 13:17:13 +0100

    first working version but serious bugs

--- main.py
+++ main.py
@@ -1,2 +1,2 @@
 import speedsmart_tools
-speedsmart_tools.restore_full_length("original.csv", "latest.csv")
+speedsmart_tools.restore_full_length("original.csv", "latest.csv", "fulllength.csv")
--- speedsmart_tools.py
+++ speedsmart_tools.py
@@ -1,13 +1,12 @@
 import csv
 
-def restore_full_length(originaltable, latesttable):
+def restore_full_length(originaltable, latesttable, newtablename):
     # This function will give you a full-length SpeedSmart table
     with open(originaltable, "r") as ss_original_file:
         with open (latesttable, "r") as ss_latest_file:
             # Get the first speedtest present in the file
             firsttest = list(csv.reader(ss_latest_file))[1]
             firsttest.pop(0)
-            print(firsttest)
             count = 1
             inserting = []
             for row in csv.reader(ss_original_file):
@@ -17,4 +16,19 @@ def restore_full_length(originaltable, latesttable):
                     print("Found first test in original file, row ", count)
                     break
                 count += 1
-    print(inserting)
+    writing = ""
+    for rowlist in inserting:
+        for index, item in enumerate(rowlist):
+            writing = writing+"\""+item+"\""
+            if index == len(rowlist)-1:
+                writing = writing+"\n"
+            else:
+                writing = writing+","
+    with open (latesttable, "r") as ss_latest_file:    
+        ss_latest_file.readline()
+        ss_latest_file.readline()
+        writing = writing+ss_latest_file.read()
+    with open (newtablename, "w") as combinedfile:
+        combinedfile.write(writing)
+        print("Written full table to "+newtablename)
+
