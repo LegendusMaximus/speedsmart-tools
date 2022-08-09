4a6c298da71cae7cf34430b09b0f70a0b800aff1
Legendusmaximus <tapsandsinkstheo@gmail.com>
2022-08-09 20:06:50 +0100

    updating webapp

--- webapp/speedsmartdata/stats.py
+++ webapp/speedsmartdata/stats.py
@@ -1,1 +1,9 @@
 import speedsmart_tools
+import speedsmart_config as config
+import csv
+
+def table_length():
+    with open(config.fulllength, "r") as table:
+        treader = list(csv.reader(table))
+        lengthoftable = len(treader)-1
+    return lengthoftable
