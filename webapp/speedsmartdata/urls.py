bee74c51cac560218d5b22c66ca0b0fe6a20980f
Legendusmaximus <tapsandsinkstheo@gmail.com>
2022-08-10 11:32:55 +0100

    web app working

--- webapp/speedsmartdata/urls.py
+++ webapp/speedsmartdata/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name='index'),
+    path("sortdown/",views.sortdown,name="sortdown"),
+    path("sortup/",views.sortup,name="sortup"),
+    path("fulllength/",views.fulllength,name="fulllength"),
+    path("sorted/",views.sorted,name="sorted"),
+    path("averages/",views.averages,name="averages"),
 ]
--- webapp/speedsmartdata/views.py
+++ webapp/speedsmartdata/views.py
@@ -5,10 +5,12 @@ import stats as stats
 import speedsmart_average
 import speedsmart_config as config
 import speedsmart_tools
+import os
+import shutil
 
 def index(request):
     completed = 0
-    if request.method == "POST":
+    if request.method == "POST" and request.FILES["ssfile"]:
         file = request.FILES["ssfile"]
         fs = FileSystemStorage()
         if fs.exists("files/"+file.name):
@@ -40,3 +42,44 @@ def index(request):
     length = stats.table_length()
     downloadavg, uploadavg, pingavg = stats.average()
     return render(request, "speedsmartdata/index.html", {"length": length,"downloadavg": downloadavg,"uploadavg": uploadavg, "pingavg": pingavg, "completed": completed})
+
+def sortdown(request):
+    if request.method == "POST":
+        speedsmart_tools.sort_by_speed(0)
+        return render(request, "speedsmartdata/sort.html", {"type": "download"})
+
+def sortup(request):
+    if request.method == "POST":
+        speedsmart_tools.sort_by_speed(1)
+        return render(request, "speedsmartdata/sort.html", {"type": "upload"})
+
+def fulllength(request):
+    if request.method == "POST":
+        return download(config.fulllength)
+    else:
+        return HttpResponse("This download cannot be completed as it was not initiated through the correct button on the homepage.")
+
+def sorted(request):
+    if request.method == "POST":
+        return download("sorting/speed.csv")
+    else:
+        return HttpResponse("This download cannot be completed as it was not initiated through the correct button on the homepage.")
+
+def averages(request):
+    if request.method == "POST":
+        shutil.make_archive("../averages", 'zip', "../averages/")
+        return download("averages.zip")
+    else:
+        return HttpResponse("This download cannot be completed as it was not initiated through the correct button on the homepage.")
+
+
+
+
+def download(path):
+    file_path = "../"+path
+    if os.path.exists(file_path):
+        with open(file_path, 'rb') as fh:
+            response = HttpResponse(fh.read(), content_type="application/vnd.ms-excel")
+            response['Content-Disposition'] = 'inline; filename=' + os.path.basename(file_path)
+            return response
+    raise Http404
