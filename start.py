3775bff7ecf7f457392373a8bdf326f55df1cd99
Legendusmaximus <tapsandsinkstheo@gmail.com>
2022-08-13 19:29:53 +0100

    starting work on drive integration

--- start.py
+++ start.py
@@ -2,6 +2,11 @@
 import speedsmart_tools
 import speedsmart_config as config
 
+try:
+    import speedsmart_secrets
+except:
+    print("Warning: Please rename speedsmart_secrets_example.py to speedsmart_secrets.py before using this tool. Failing to do this will give you problems down the line.")
+
 print("Welcome to SpeedSmart Tools!")
 print("")
 print("====================")
