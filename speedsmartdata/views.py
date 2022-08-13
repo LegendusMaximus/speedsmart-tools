871ab3c2bba2209c4fc3d11e31ba6f6bb963326a
Legendusmaximus <tapsandsinkstheo@gmail.com>
2022-08-13 16:14:08 +0100

    fixed file download error

--- speedsmartdata/views.py
+++ speedsmartdata/views.py
@@ -78,7 +78,7 @@ def averages(request):
 def download(file_path):
     if os.path.exists(file_path):
         with open(file_path, 'rb') as fh:
-            response = HttpResponse(fh.read(), content_type="application/vnd.ms-excel")
+            response = HttpResponse(fh.read(), content_type="text/plain")
             response['Content-Disposition'] = 'inline; filename=' + os.path.basename(file_path)
             return response
     raise Http404
