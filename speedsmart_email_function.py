9dd34d748f6093d7027928a68529cdfacb53c4cf
Legendusmaximus <tapsandsinkstheo@gmail.com>
2022-08-18 18:52:48 +0100

    fixed bug

--- speedsmart_email_function.py
+++ speedsmart_email_function.py
@@ -25,27 +25,27 @@ def check():
         for idx, attachment in enumerate(message.attachments):
             try:
                 att_fn = attachment.get('filename')
-                download_path = f"{download_folder}/{att_fn}"
-                print(download_path)
-                with open(download_path, "wb") as fp:
-                    fp.write(attachment.get('content').read())
-                speedsmart_tools.restore_full_length(config.original, download_path, config.fulllength)
-                if config.andnetworks == 0:
-                    print("Skipping and replacing")
-                else:
-                    print("Starting and replacing")
-                    speedsmart_tools.and_replacing(config.fulllength)
-                if config.hashnetworks == 0:
-                    print("Skipping hashtag replacing")
-                else:
-                    print("Starting hashtag replacing")
-                    speedsmart_tools.hashtag_replacing(config.fulllength)
-                if config.count == 1:
-                    print("Restoring count column")
-                    speedsmart_tools.restore_count(config.fulllength)
-                print("Finished!")
-                speedsmart_attach.attach_and_send()
-
+                if ".csv" in att_fn:
+                    download_path = f"{download_folder}/{att_fn}"
+                    print(download_path)
+                    with open(download_path, "wb") as fp:
+                        fp.write(attachment.get('content').read())
+                    speedsmart_tools.restore_full_length(config.original, download_path, config.fulllength)
+                    if config.andnetworks == 0:
+                        print("Skipping and replacing")
+                    else:
+                        print("Starting and replacing")
+                        speedsmart_tools.and_replacing(config.fulllength)
+                    if config.hashnetworks == 0:
+                        print("Skipping hashtag replacing")
+                    else:
+                        print("Starting hashtag replacing")
+                        speedsmart_tools.hashtag_replacing(config.fulllength)
+                    if config.count == 1:
+                        print("Restoring count column")
+                        speedsmart_tools.restore_count(config.fulllength)
+                    print("Finished!")
+                    speedsmart_attach.attach_and_send()
             except:
                 print(traceback.print_exc())
 
